8393d43b3941b89857ec96f90c957f2ef5eef612
salvanescki <salvanescki.unq@gmail.com>
2025-02-06 20:43:06 -0300

    fix: network error in auth/register

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      audience: process.env.AWS_COGNITO_COGNITO_CLIENT_ID,
+      audience: process.env.AWS_COGNITO_CLIENT_ID,
       issuer: process.env.AWS_COGNITO_AUTHORITY,
       algorithms: ['RS256'],
       secretOrKeyProvider: passportJwtSecret({
